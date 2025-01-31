d58aa3bb3b9a89cdb5812f13305c699141144da8
Michal Gora <arog.lahcim@gmail.com>
2025-01-31 13:59:38 +0100

    feat: add keyboard support

--- app/app.js
+++ app/app.js
@@ -1,16 +1,49 @@
-window.addEventListener("load", async () => {
-  const searchInput = document.querySelector("#search");
-  const vscodes = [...document.querySelectorAll(".vscode").values()];
-  searchInput.addEventListener("input", () => {
-    vscodes
+(() => {
+  let selectedIndex = -1;
+  
+  window.addEventListener("load", async () => {
+    const searchInput = document.querySelector("#search");
+    const vscodes = [...document.querySelectorAll(".vscode").values()];
+    
+    searchInput.addEventListener("input", () => {
+      selectedIndex = -1;
+      vscodes
       .filter((link) => link.textContent.includes(searchInput.value))
       .forEach((link) => {
         link.style.display = "";
       });
-    vscodes
+      vscodes
       .filter((link) => !link.textContent.includes(searchInput.value))
       .forEach((link) => {
         link.style.display = "none";
       });
+    });
+    
+    searchInput.addEventListener('focus', () => {
+      searchInput.setSelectionRange(searchInput.value.length, searchInput.value.length);
+      selectedIndex = -1;
+    });
   });
-});
+  
+  window.addEventListener("keydown", async (event) => {
+    const searchInput = document.querySelector("#search");
+    const vscodes = [...document.querySelectorAll(".vscode:not([style*=\"display: none\"])").values()];
+    
+    if (event.code === 'Escape') {
+      searchInput.focus();
+      
+    } else if (event.code === 'ArrowDown') {
+      selectedIndex = Math.min(selectedIndex + 1, vscodes.length - 1);
+      vscodes.at(selectedIndex).focus();
+      
+    } else if (event.code === 'ArrowUp') {
+      selectedIndex = Math.max(selectedIndex - 1, -1);
+      if (selectedIndex === -1) {
+        searchInput.focus();
+      } else {
+        vscodes.at(selectedIndex).focus();
+      }
+    }
+  })
+  
+})();
